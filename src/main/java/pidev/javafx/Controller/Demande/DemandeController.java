aaf1b6da508dc7a73166067baea0cc0223d86fbe
khalil <khalil.rmila@esprit.tn>
2024-01-29 21:35:08 +0100

    update + first intrafce

---
+++ src/main/java/pidev/javafx/Controller/Demande/DemandeController.java
@@ -0,0 +1,32 @@
+package pidev.javafx.Controller.Demande;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class DemandeController {
+
+    public Pane box1;
+
+    public Button showmore;
+    public Button showmore1;
+    public Button showmore2;
+
+    public Pane box2;
+    public Pane box3;
+
+
+    public void  getBoton_on_click_add() {
+        box1.setOnMouseEntered(event -> showmore.setVisible(true));
+        box1.setOnMouseExited(event -> showmore.setVisible(false));
+        //boton_on_click_add.setVisible(true);
+    }
+    public void  getBoton_on_click_add1() {
+        box2.setOnMouseEntered(event -> showmore1.setVisible(true));
+        box2.setOnMouseExited(event -> showmore1.setVisible(false));
+        //boton_on_click_add.setVisible(true);
+    }
+    public void  getBoton_on_click_add2() {
+        box3.setOnMouseEntered(event -> showmore2.setVisible(true));
+        box3.setOnMouseExited(event -> showmore2.setVisible(false));
+        //boton_on_click_add.setVisible(true);
+    }
+}

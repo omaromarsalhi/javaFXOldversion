750031e3c79b2610af3ce639c770d9c6e825f30a
omar salhi <omar.salhi@esprit.tn>
2024-02-01 00:32:04 +0100

    commit

--- src/main/java/pidev/javafx/Controller/MarketPlace/ItemController.java
+++ src/main/java/pidev/javafx/Controller/MarketPlace/ItemController.java
@@ -5,6 +5,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import pidev.javafx.Model.MarketPlace.Bien;
 import pidev.javafx.Model.MarketPlace.Categorie;
 import pidev.javafx.Model.MarketPlace.Fruit;
@@ -25,6 +28,12 @@ public class ItemController {
     private Label priceLable;
     @FXML
     private ImageView img;
+    @FXML
+    private HBox itemInfo;
+    @FXML
+    private AnchorPane anchorPaneItem;
+    @FXML
+    private VBox bascInfoItems;
 
 //    @FXML
 //    private void click(MouseEvent mouseEvent) {
@@ -35,6 +44,9 @@ public class ItemController {
 //    private MyListener myListener;
 
     public void setData(Bien bien, MyListener myListener) {
+        itemInfo.setPrefHeight( 0 );
+        bascInfoItems.setPrefHeight( 330 );
+        anchorPaneItem.setPrefHeight( 340 );
         this.bien = bien;
 //        this.myListener = myListener;
         nameLabel.setText(bien.getName());
@@ -45,6 +57,22 @@ public class ItemController {
         categoryLable.setText(bien.getCategorie().name());
         Image image = new Image(getClass().getResourceAsStream(bien.getImgSource()));
         img.setImage(image);
+        itemInfo.setVisible( false );
+    }
+    public void showTransitionInfo(Boolean state){
+        if(state){
+            anchorPaneItem.setPrefHeight( 360 );
+            bascInfoItems.setPrefHeight( 308 );
+            itemInfo.setPrefHeight(55 );
+        }
+        else{
+            itemInfo.setPrefHeight( 0 );
+            bascInfoItems.setPrefHeight( 330 );
+            anchorPaneItem.setPrefHeight( 340 );
+        }
+
+        itemInfo.setVisible( state );
+
     }
 
 }
--- src/main/java/pidev/javafx/Controller/MarketPlace/MarketController.java
+++ src/main/java/pidev/javafx/Controller/MarketPlace/MarketController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.chart.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
@@ -16,32 +17,45 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class MarketController implements Initializable {
-//    @FXML
-//    private VBox chosenFruitCard;
-//
-//    @FXML
-//    private Label fruitNameLable;
-//
-//    @FXML
-//    private Label fruitPriceLabel;
-//
-//    @FXML
-//    private ImageView fruitImg;
 
+    @FXML
+    private GridPane grid;
     @FXML
     private ScrollPane scroll;
     @FXML
-    private Button addBien;
+    private LineChart<?,?> lineChart;
     @FXML
-    private GridPane grid;
+    private StackedBarChart<?, ?> stachBarChart;
+    @FXML
+    private Button addBien;
+
 
     private List<Bien> biens = new ArrayList<>();
     private Image image;
     private MyListener myListener;
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        showGridPane();
+        getCharts();
+    }
+
+
+
+    public void getProduct(AnchorPane item,ItemController itemController){
+        item.hoverProperty().addListener( (observable, oldValue, show)->{
+            itemController.showTransitionInfo( show );
+        } );
+    }
+
 
 
     private List<Bien> getData() {
@@ -54,9 +68,60 @@ public class MarketController implements Initializable {
         return biens;
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    private void getCharts() {
+        lineChart.getYAxis().setLabel("Product Number");
+        var serie = new XYChart.Series();
+        serie.setName("This week");
+        serie.getData().add(new XYChart.Data("Mo", 100 ));
+        serie.getData().add(new XYChart.Data("Tu", 45 ));
+        serie.getData().add(new XYChart.Data("We", 20 ));
+        serie.getData().add(new XYChart.Data("Th", 110 ));
+        serie.getData().add(new XYChart.Data("Fr", 60 ));
+        serie.getData().add(new XYChart.Data("Sa", 66 ));
+        serie.getData().add(new XYChart.Data("Su", 13 ));
 
+        lineChart.getData().addAll(serie);
+        lineChart.getStylesheets().add( String.valueOf( getClass().getResource("/style/lineChartStyle.css") ) );
+
+
+        stachBarChart.getYAxis().setLabel("value in $");
+        var selled = new XYChart.Series();
+        var purshased = new XYChart.Series();
+        var traded = new XYChart.Series();
+
+        selled.setName("Selled");
+        selled.getData().add(new XYChart.Data("Mo", 100 ));
+        selled.getData().add(new XYChart.Data("Tu", 45 ));
+        selled.getData().add(new XYChart.Data("We", 20 ));
+        selled.getData().add(new XYChart.Data("Th", 110 ));
+        selled.getData().add(new XYChart.Data("Fr", 60 ));
+        selled.getData().add(new XYChart.Data("Sa", 66 ));
+        selled.getData().add(new XYChart.Data("Su", 13 ));
+
+        purshased.setName("Purshased");
+        purshased.getData().add(new XYChart.Data("Mo", 10 ));
+        purshased.getData().add(new XYChart.Data("Tu", 80 ));
+        purshased.getData().add(new XYChart.Data("We", 70 ));
+        purshased.getData().add(new XYChart.Data("Th", 31 ));
+        purshased.getData().add(new XYChart.Data("Fr", 16 ));
+        purshased.getData().add(new XYChart.Data("Sa", 120 ));
+        purshased.getData().add(new XYChart.Data("Su", 95 ));
+
+        traded.setName("Traded");
+        traded.getData().add(new XYChart.Data("Mo", 59 ));
+        traded.getData().add(new XYChart.Data("Tu", 45 ));
+        traded.getData().add(new XYChart.Data("We", 73 ));
+        traded.getData().add(new XYChart.Data("Th", 110 ));
+        traded.getData().add(new XYChart.Data("Fr", 120 ));
+        traded.getData().add(new XYChart.Data("Sa", 88 ));
+        traded.getData().add(new XYChart.Data("Su", 14 ));
+
+
+        stachBarChart.getData().addAll(selled,purshased,traded);
+        stachBarChart.getStylesheets().add( String.valueOf( getClass().getResource("/style/lineChartStyle.css") ) );
+
+    }
+    public void showGridPane(){
         biens.addAll(getData());
 //        if (fruits.size() > 0) {
 //            setChosenFruit(fruits.get(0));
@@ -70,7 +135,6 @@ public class MarketController implements Initializable {
         int column = 0;
         int row = 1;
         try {
-            System.out.println(biens.size());
             for (int i = 0; i < biens.size(); i++) {
                 FXMLLoader fxmlLoader = new FXMLLoader();
                 fxmlLoader.setLocation(getClass().getResource("/fxml/marketPlace/item.fxml"));
@@ -78,12 +142,11 @@ public class MarketController implements Initializable {
 
                 ItemController itemController = fxmlLoader.getController();
                 itemController.setData(biens.get(i),myListener);
-
+                getProduct(anchorPane,itemController);
                 if (column == 3) {
                     column = 0;
                     row++;
                 }
-
 //                grid.setGridLinesVisible( true );
                 grid.setHgap( 40 );
                 grid.setVgap( 40 );
@@ -107,8 +170,8 @@ public class MarketController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
+    }
 
 
 

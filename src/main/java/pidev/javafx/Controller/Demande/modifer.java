550f8fc658359f90360910756b00909413a4cf62
khalil <khalil.rmila@esprit.tn>
2024-02-13 16:13:13 +0100

    affiche sayer

--- src/main/java/pidev/javafx/Controller/Demande/modifer.java
+++ src/main/java/pidev/javafx/Controller/Demande/modifer.java
@@ -4,18 +4,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import pidev.javafx.Controller.Entity.Reclamation;
 import pidev.javafx.Controller.Service.ServiceReclamation;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class modifer {
 
     @FXML
-    public VBox box;
+    public ListView lista;
     ServiceReclamation si = new ServiceReclamation();
 //    public void initialize() {
 //        try {
@@ -33,19 +36,24 @@ public class modifer {
 //            e.printStackTrace();
 //        }
 public void initialize() {
-    try {
-        // Get the data from the database
+    ServiceReclamation service = new ServiceReclamation();
+    List<Reclamation> reclamations = new ArrayList<>(service.getAll());
 
-        VBox box = new VBox();
+    // Add the Reclamation objects to the ListView
+    lista.getItems().addAll(reclamations);
 
-        // Iterate through the result set and create labels
-        for (String privateKey : si.getAllPrivateKeys()) {
-            Label label = new Label(privateKey);
-            box.getChildren().add(label);
+    // Optionally, you can set a custom cell factory to control how each Reclamation is displayed
+    lista.setCellFactory(reclamationListView -> new ListCell<Reclamation>() {
+        @Override
+        protected void updateItem(Reclamation reclamation, boolean empty) {
+            super.updateItem(reclamation, empty);
+            if (empty || reclamation == null) {
+                setText(null);
+            } else {
+                setText(reclamation.getPrivateKey() + " " + reclamation.getSubject() + " " + reclamation.getTitre());
+            }
         }
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
+    });
 }
 
 }
